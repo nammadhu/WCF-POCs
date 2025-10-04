a002d7888e150f7d068164e6a2787f94a0d79e1b
Madhu <nammadhu@outlook.in>
2025-10-04 13:49:40 +0530

    migrated to .net core

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddServiceModelServices();
+builder.Services.AddServiceModelMetadata();
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+var app = builder.Build();
+
+app.UseServiceModel(serviceBuilder =>
+{
+    serviceBuilder.AddService<Service1>();
+    serviceBuilder.AddServiceEndpoint<Service1, IService1>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/Service1.svc");
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpsGetEnabled = true;
+});
+
+app.Run();
